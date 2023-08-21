4ec0fd28dc5b5c887466a494cd2c990160f0b375
manju047 <141726696+manju047@users.noreply.github.com>
2023-08-21 14:01:17 +0530

    Update stk_entry.php

--- stk_entry.php
+++ stk_entry.php
@@ -1,9 +1,5 @@
 <?php
 require 'connection.php';
-if(!empty($_SESSION["id"]))
-{
-  header("Location: stock_entry_reg.html");
-}
 
   $stkid = $_POST["stkid"];
   $stkname=$_POST["stkname"];
@@ -21,25 +17,6 @@ if(mysqli_query($connection,$sqlquery))
   echo "<script> alert('Insertion Unsucessfull'); </script>".mysqli_error($connection);
 
   }
-  
-
-
-
-
- //prepare and bind the statement
-/*$stmt=$connection->prepare($sqlquery);
- $stmt->bind_param($id,$name, $qty,$vname,$price);
- //execut the statement
- if($stmt->execute())
- {
-  echo "New record is inserted sucessfully";
- }
- else
- {
-  echo "Error".$sqlquery."<br>".$connection->error;
- }
-
- //close the statement and connection
- //$stmt->close();*/
+  header("Location: stock_entry_reg.html");
  $connection->close();
  ?>
