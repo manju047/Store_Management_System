024f36f011d4772ab943fa9d442af3740a6219bf
manju047 <141726696+manju047@users.noreply.github.com>
2023-09-07 18:49:59 +0800

    Update stk_entry.php

--- stk_entry.php
+++ stk_entry.php
@@ -3,20 +3,23 @@ require 'connection.php';
 
   $stkid = $_POST["stkid"];
   $stkname=$_POST["stkname"];
-  $stkquantity =$_POST["stkquantity"];
+  $stkquantity=$_POST["stkquantity"];
   $stkvendor=$_POST["stkvendor"];
   $stkprice=$_POST["stkprice"];
-  
-  $sqlquery = "INSERT INTO stk_entry VALUES ('$stkid', '$stkname', '$stkquantity', '$stkvendor','$stkprice')";
-if(mysqli_query($connection,$sqlquery))
-{
-  echo "<script> alert('New record inserted Successfully'); </script>";
-}
-  else
-  {
-  echo "<script> alert('Insertion Unsucessfull'); </script>".mysqli_error($connection);
 
+  $sqlquery = "INSERT INTO stk_entry (stkid,stkname,stkquantity,stkvendor,stkprice) VALUES ('$stkid', '$stkname', '$stkquantity', '$stkvendor','$stkprice')";
+  if ($connection->query($sqlquery) == TRUE)
+   {
+    // Data inserted successfully, generate popup message
+    echo '<script type ="text/JavaScript">';  
+    echo 'alert(" New Record Inserted Sucessfully..!! ")';  
+    echo '</script>'; 
+     }  
+  else 
+  {
+    echo "Error: " . $sql . "<br>" . $conn->error;
   }
-  header("Location: stock_entry_reg.html");
+
  $connection->close();
+ 
  ?>
