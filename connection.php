13a3ee0089408ac6d676a27601383ad536ff50fd
manju047 <141726696+manju047@users.noreply.github.com>
2023-09-07 18:52:10 +0800

    Update connection.php

--- connection.php
+++ connection.php
@@ -2,19 +2,15 @@
 $hostname='localhost';  //127.0.0.1
 $username='root';
 $password='';
-$database='reglog';
+$database='store';
 
 //create a connection 
 $connection=mysqli_connect($hostname,$username,$password,$database);
 
-//ceck if the connection was succesfull
-if(!$connection)
-{
-    die('connection failed:'.mysqli_connect_error());
+//check if the connection was succesfull
+if ($connection->connect_error) {
+    die("Connection failed: " . $connection->connect_error);
+  }
+  echo "";
+  ?>
 
-}
-else
-{
-     
-}
-?>
